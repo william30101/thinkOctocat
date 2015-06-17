10bb17e274acbdb7efc7da1b4eab0dda729aa4f2
william <william@octocat-MS-7922.(none)>
2015-06-17 14:55:11 +0800

    initial version

---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_NAME' => 'thinkcmf',
+    'DB_USER' => 'root',
+    'DB_PWD' => '1qaz@WSX',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'sp_',
+    //密钥
+    "AUTHCODE" => 'JxtWU3MFR46zzE5AWD',
+    //cookies
+    "COOKIE_PREFIX" => '9DHAvu_',
+);
+?>
